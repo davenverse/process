04056b3c7ac83cde973bd4ae4dd417dbd4f28788
Christopher Davenport <chris@christopherdavenport.tech>
2022-01-21 22:07:11 -0800

    Use the primary platform

--- core/jvm/src/main/scala/io/chrisdavenport/process/ChildProcessCompanionPlatform.scala
+++ core/jvm/src/main/scala/io/chrisdavenport/process/ChildProcessCompanionPlatform.scala
@@ -6,15 +6,12 @@ import cats.syntax.all._
 trait ChildProcessCompanionPlatform {
 
   private[process] class ChildProcessImpl[F[_]: Async] extends ChildProcess[F]{
-    def exec(command: String, args: List[String]): F[String] = Sync[F].delay{
-      import sys.process._
-      (command :: args).!!
-    }
-    def execCode(command: String, args: List[String]): F[Int] = Sync[F].delay{
-      import sys.process._
-      // TODO Get rid of this as it sends stdout to the process console
-      (command :: args).! 
-    }
+    def exec(command: String, args: List[String]): F[String] = 
+      spawn(Process(command, args))
+        .flatMap(_.stdout.through(fs2.text.utf8.decode).compile.string)
+
+    def execCode(command: String, args: List[String]): F[Int] = 
+      spawn(Process(command, args)).flatMap(_.getExitCode)
 
     val readBufferSize = 4096
 
