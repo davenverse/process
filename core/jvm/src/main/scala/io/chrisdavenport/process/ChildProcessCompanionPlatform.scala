0322cf2d90aee29e602fe23a0e2948c2d74f3ea4
Christopher Davenport <chris@christopherdavenport.tech>
2022-01-21 21:58:37 -0800

    Add Java Spawn Implementation

--- core/jvm/src/main/scala/io/chrisdavenport/process/ChildProcessCompanionPlatform.scala
+++ core/jvm/src/main/scala/io/chrisdavenport/process/ChildProcessCompanionPlatform.scala
@@ -1,8 +1,7 @@
 package io.chrisdavenport.process
 
 import cats.effect._
-
-
+import cats.syntax.all._
 
 trait ChildProcessCompanionPlatform {
 
@@ -17,7 +16,35 @@ trait ChildProcessCompanionPlatform {
       (command :: args).! 
     }
 
-    def spawn(process: Process): F[RunningProcess[F]] = ???
+    val readBufferSize = 4096
+
+    def spawn(process: Process): F[RunningProcess[F]] = Async[F].interruptible{
+      import scala.jdk.CollectionConverters._
+      val p = new java.lang.ProcessBuilder((process.command :: process.args).asJava).start()//.directory(new java.io.File(wd)).start()
+      val done = Async[F].fromCompletableFuture(Sync[F].delay(p.onExit()))
+
+
+      new RunningProcess[F] {
+        def getExitCode: F[Int] = done.map(_ => p.exitValue())
+        def writeToStdIn(s: fs2.Stream[F,Byte]): F[Unit] =  s
+                        .through(fs2.io.writeOutputStream[F](Sync[F].interruptible(p.getOutputStream())))
+                        .compile
+                        .drain
+        
+        def terminate: F[Unit] = Sync[F].interruptible(p.destroy())
+        
+        def stdout: fs2.Stream[F,Byte] = fs2
+          .io
+          .readInputStream[F](Sync[F].interruptible(p.getInputStream()), chunkSize = readBufferSize)
+        
+        def stderr: fs2.Stream[F,Byte] = fs2
+          .io
+          .readInputStream[F](Sync[F].blocking(p.getErrorStream()), chunkSize = readBufferSize)
+          // Avoids broken pipe - we cut off when the program ends.
+          // Users can decide what to do with the error logs using the exitCode value
+          .interruptWhen(done.void.attempt)
+      }
+    }
   }
   
 }
