c15b8b4da83c02185e48078167f5d314ef343a64
Christopher Davenport <chris.davenport@banno.com>
2022-01-25 13:51:18 -0800

    Use Process now that its easier

--- core/js/src/main/scala/io/chrisdavenport/process/ChildProcessCompanionPlatform.scala
+++ core/js/src/main/scala/io/chrisdavenport/process/ChildProcessCompanionPlatform.scala
@@ -15,9 +15,9 @@ import scala.scalajs.js.typedarray.{ArrayBuffer, TypedArrayBuffer, Uint8Array}
 trait ChildProcessCompanionPlatform {
 
   private[process] class ChildProcessImpl[F[_]: Async] extends ChildProcess[F]{
-    def execSimple(command: String, args: List[String]): F[(String, String)] = Async[F].async{(cb: Either[Throwable, (String, String)] => Unit) => 
+    def execSimple(proces: Process): F[(String, String)] = Async[F].async{(cb: Either[Throwable, (String, String)] => Unit) => 
       Async[F].delay{
-        val commandS = command ++ { if (args.nonEmpty) " " else "" } ++ args.mkString(" ")
+        val commandS = proces.command ++ { if (proces.args.nonEmpty) " " else "" } ++ proces.args.mkString(" ")
         def method(error: internal.childProcessMod.ExecException, stdout: String, stderr: String): Unit = {
           if (error != null) {
             cb(Left(new Throwable(s"Failed Process Execution, ${error.cmd.toOption} ${error.message}")))
@@ -28,20 +28,20 @@ trait ChildProcessCompanionPlatform {
       }
     }
 
-    def execCode(command: String, args: List[String]): F[Int] = Async[F].async{(cb: Either[Throwable, Int] => Unit) => 
+    def execCode(proces: Process): F[Int] = Async[F].async{(cb: Either[Throwable, Int] => Unit) => 
       Async[F].delay{
         def onExit(int: Double, signal: String): Unit = 
           cb(Either.right(int.toInt))
         def onError(error: scalajs.js.Error): Unit = 
           cb(Either.left(new Throwable(s"${error.name}: ${error.message}")))
-        val process = internal.childProcessMod.spawn(command, args.toJSArray)
+        val process = internal.childProcessMod.spawn(proces.command, proces.args.toJSArray)
         process.addListener_exit(internal.childProcessMod.strings.exit, onExit)
         process.addListener_error(internal.childProcessMod.strings.error, onError)
         Some(Async[F].delay(process.kill()).void)
       }
     }
     
-    def exec(command: String, args: List[String]): F[String] = execSimple(command, args).map(_._1)
+    def exec(process: Process): F[String] = execSimple(process).map(_._1)
 
     def spawn(process: Process): F[RunningProcess[F]] = Async[F].delay{
       // Initial State
